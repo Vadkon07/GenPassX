a069f18185381adb074375a41c39c042ffde68fc
Vadkon07 <vadkon2007@gmail.com>
2024-10-24 04:48:40 -0400

    update

--- main.c
+++ main.c
@@ -13,10 +13,19 @@ void generate_password(char *password, int length) {
 }
 
 // Callback function for the button
-void on_generate_clicked(GtkWidget *widget, gpointer entry) {
-    char password[13]; // Password length: 12 + 1 for the null terminator
-    generate_password(password, 12);
-    gtk_entry_set_text(GTK_ENTRY(entry), password);  // Fix the argument issue
+void on_generate_clicked(GtkWidget *widget, gpointer data) {
+    GtkWidget **widgets = (GtkWidget **)data;
+    GtkWidget *entry = widgets[0];
+    GtkWidget *length_entry = widgets[1];
+    
+    const char *length_str = gtk_entry_get_text(GTK_ENTRY(length_entry));
+    int length = atoi(length_str);
+    if (length <= 0) length = 12;  // Default length
+    
+    char *password = (char *)malloc((length + 1) * sizeof(char));
+    generate_password(password, length);
+    gtk_entry_set_text(GTK_ENTRY(entry), password);
+    free(password);
 }
 
 int main(int argc, char *argv[]) {
@@ -26,6 +35,7 @@ int main(int argc, char *argv[]) {
     GtkWidget *window;
     GtkWidget *button;
     GtkWidget *entry;
+    GtkWidget *length_entry;
     GtkWidget *box;
     
     // Create a new window
@@ -38,13 +48,19 @@ int main(int argc, char *argv[]) {
     box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), box);
     
-    // Create an entry widget
+    // Create an entry widget for the generated password
     entry = gtk_entry_new();
     gtk_box_pack_start(GTK_BOX(box), entry, FALSE, FALSE, 5);
     
+    // Create an entry widget for the length input
+    length_entry = gtk_entry_new();
+    gtk_entry_set_placeholder_text(GTK_ENTRY(length_entry), "Enter length (default 12)");
+    gtk_box_pack_start(GTK_BOX(box), length_entry, FALSE, FALSE, 5);
+    
     // Create a button widget
     button = gtk_button_new_with_label("Generate Password");
-    g_signal_connect(button, "clicked", G_CALLBACK(on_generate_clicked), entry);
+    GtkWidget *widgets[] = {entry, length_entry};
+    g_signal_connect(button, "clicked", G_CALLBACK(on_generate_clicked), widgets);
     gtk_box_pack_start(GTK_BOX(box), button, FALSE, FALSE, 5);
     
     // Show all widgets
@@ -53,6 +69,8 @@ int main(int argc, char *argv[]) {
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
     
     gtk_main();
+
+    g_free(widgets);
     
     return 0;
 }
